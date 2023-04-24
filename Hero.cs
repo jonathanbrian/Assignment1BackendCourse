1c07e043aff937d90cea01a72fba0c837d535a44
jegerbetty <115633564+jegerbetty@users.noreply.github.com>
2023-04-24 15:29:39 +0200

    Created Hero Classes
    Created Hero classes, and added description for shared attributes for all hero classes in the main (parent/base) hero class

---
+++ Hero.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1BackendCourse
+{
+    internal class Hero
+    {
+        //Share attributes for all heroes:
+        //Name
+        //Level - all heroes start at level 1
+        //LevelAttribtues - total from all levels
+        //Equipment - holds currently equipped items
+        //ValidWeaponTypes – a list of weapon types a hero can equip based on their subclass
+        //ValidArmorTypes - a list of armor types a hero can equip based on their subclass
+
+        /*Public facing methods: 
+         * Constructor – each hero is created by passing just a name.
+         * • LevelUp – increases the level of a character by 1 and increases their LevelAttributes
+         * • Equip – two variants, for equipping armor and weapons
+         * • Damage – damage is calculated on the fly and not stored
+         * • TotalAttributes – calculated on the fly and not stored
+         * • Display – details of Hero to be displayed
+        */
+}
+}
