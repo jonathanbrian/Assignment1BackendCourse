48fc5273e2e05c8e2c833c864a47cfe1ac66ec5b
jegerbetty <115633564+jegerbetty@users.noreply.github.com>
2023-04-24 15:33:48 +0200

    Create HeroAttribute with instructions

---
+++ HeroAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1BackendCourse
+{
+    internal class HeroAttribute
+    {
+        /*Strength – determines the physical strength of the character.
+        *• Dexterity – determines the characters ability to attack with speed and nimbleness.
+        *• Intelligence – determines the characters affinity with magic.
+        *This class should have methods to add two instances together and return the sum 
+        *OR increase the one instance by a specified amount. 
+        *Which to use is up to you on what best suits your solution.
+        *NOTE: This class is used as the datatype for both LevelAttributes.
+        */
+    }
+}
